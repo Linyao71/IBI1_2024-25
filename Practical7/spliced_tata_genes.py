59c8a77014fb6f53daa49e38dd91f266ff760441
Linyao71 <wyp153246@163.com>
2025-04-01 15:24:35 +0800

    spliced tata gene

---
+++ Practical7/spliced_tata_genes.py
@@ -0,0 +1,43 @@
+import sys
+import re
+
+option = ['GTAG', 'GCAG', 'ATAC']
+
+print("possible splice donor/acceptor combinations: GTAG, GCAG, ATAC")
+while True:
+    splice = input("input a splice combination: ").strip().upper()
+    if splice in option:
+        break
+    print("Meaningless input. Please choose from GTAG, GCAG, ATAC.")
+
+input = open ('tata_genes.fa','r')
+tata = re.compile(r'TATA[AT][AT]', re.IGNORECASE)
+output1 = f'{splice}_spliced_genes.fa'
+output = open (output1,'w')
+currentseq = []
+currentname = "unknown_gene"
+
+for line in input:
+    if bool(re.match ('>', line)):  
+        if currentseq:
+            full = ''.join(currentseq)
+
+            if tata.search(full):
+                if re.search(splice, full) and re.search(r'TATA[AT][AT]', full, re.IGNORECASE):
+                     number = len(re.findall(r'TATA[AT][AT]', full, re.IGNORECASE))
+                     output.write(f'>{currentname} TATA_count={number}\n{full}\n')
+
+        currentseq = []
+        getname = re.search(r'>(\S+)', line)
+        currentname = getname.group(1) if getname else "unknown_gene"
+    
+    else:
+        currentseq.append(line.strip())
+
+if currentseq:
+        full = ''.join(currentseq)
+        if re.search(splice, full) and re.search(r'TATA[AT][AT]', full, re.IGNORECASE):
+            number = len(re.findall(r'TATA[AT][AT]', full, re.IGNORECASE))
+            output.write(f'>{currentname} TATA_count={number}\n{full}\n')
+
+print(f"Results saved to {output1}")
