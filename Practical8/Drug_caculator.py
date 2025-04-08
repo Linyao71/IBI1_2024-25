e5e7a2e08ccf23de58e6b758b088955e4801383d
Linyao71 <wyp153246@163.com>
2025-04-08 09:55:07 +0800

    durg caculator example

--- Practical8/Drug_caculator.py
+++ Practical8/Drug_caculator.py
@@ -26,3 +26,21 @@ while True:
 
 V = dosage(W,M)
 print (f"the required volume of paracetamol is {V}ml")
+
+'''running example:
+>please input the childern's weight (in kg): 
+>9
+
+>The weight provided is out of the specification, please re-enter
+>please input the childern's weight (in kg): 
+>10
+
+>possible strengths of paracetamol: 120mg/5ml or 250mg/5ml)
+>please input the required mass of niacinamide corresponding to 5ml of solution (120 or 250): 
+>270
+
+>Meaningless input. Please choose beteen 120mg/5ml and 250mg/5ml.
+>please input the required mass of niacinamide corresponding to 5ml of solution (120 or 250): 
+>120
+>the required volume of paracetamol is 1.25
+'''
