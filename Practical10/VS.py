cf73500080ebb7252a0b581720366e1d28008b92
Linyao71 <wyp153246@163.com>
2025-04-22 11:11:42 +0800

    dataset

---
+++ Practical10/VS.py
@@ -0,0 +1,39 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+dalys_data = pd.read_csv("dalys-rate-from-all-causes.csv")
+
+uk = dalys_data.loc[dalys_data.Entity == "United Kingdom", ["DALYs", "Year"]]
+'''france = dalys_data.loc[dalys_data.Entity == "France", ['DALYs', 'Year']]
+
+ukmean = uk['DALYs'].mean()
+francemean = france['DALYs'].mean()
+
+if ukmean > francemean:
+    print("The mean DALYs for the UK is larger than that of France.")
+elif ukmean < francemean:
+    print("The mean DALYs for the France is larger than that of UK.")
+else:
+    print("the mean DALYs for the UK and France are equal.")'''
+
+plt.plot(uk.Year, uk.DALYs, 
+         markersize=4,       # Kky tick size
+         markerfacecolor='Lavender', # fill color
+         markeredgecolor='#BA55D3', # border color
+         markeredgewidth=1, # border width
+         color='Purple', # line color
+         linestyle=':',         # line style
+         marker='p',          # marker type
+         label="DALYs in the UK") 
+
+plt.xticks(uk.Year,rotation=-60)
+plt.show()
+
+data_1990 = dalys_data[dalys_data.Year == 1990]
+
+plt.boxplot(data_1990.DALYs, vert=False)
+plt.title('DALYs Distribution Across Countries in 1990')
+plt.xlabel('DALYs')
+plt.show()
